7af259e0d0b19e6062c44cfdc6978a163fa49a45
kanezi <kanezi.com@gmail.com>
2025-02-03 07:18:37 +0100

    added docker compose mailpit smtp service

---
+++ src/main/java/com/kanezi/spring_one_time_token/SpringOneTimeTokenApplication.java
@@ -0,0 +1,13 @@
+package com.kanezi.spring_one_time_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOneTimeTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOneTimeTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanezi/spring_one_time_token/SpringOneTimeTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanezi.spring_one_time_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOneTimeTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
